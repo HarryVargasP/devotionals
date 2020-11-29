186beffb616273792b77a4a8643ff5a5c6c1e8ec
HarryVargasP <69601962+HarryVargasP@users.noreply.github.com>
2020-11-29 13:53:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [devotional, setDevotional] = useState(null)
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    axios.get('https://devotionals-api.herokuapp.com/api/v1/devotionals/today')
+    .then(({data}) => setDevotional(data[0]))
+    .catch((err) => setError(err))
+    .finally(() => setIsLoaded(true));
+  }, [])
+
+  if (error) {
+    return( 
+      <div className="App">
+        <h1 className="message">Error</h1>
+        <p>Hubo un error al cargar la información. Por favor, intente más tarde.</p>
+      </div>
+    )}
+  else if (isLoaded)
+    return(
+      <div className="App">
+        <h1>{devotional.title}</h1>
+        <p className="date">{devotional.date}</p>
+        <p>{devotional.vers}</p>
+        <p className="content">{devotional.content}</p>
+        <img src="/devotional.png" alt="devotional" className="image"></img>
+      </div>
+    )
+  else
+    return (
+      <div className="App">
+        <h1 className="message">Cargando...</h1>
+      </div>
+    )  
 }
 
 export default App;
